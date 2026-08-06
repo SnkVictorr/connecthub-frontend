da6562aea1dc7f2350439908de3ad13deb77b6e8
SnkVictorr <victorhugoar50@gmail.com>
2026-08-06 18:03:48 -0300

    removendo src

--- src/middlewares/asyncHandler.js
+++
@@ -1,4 +0,0 @@
-// Usado para lidar com erros em funções assíncronas no Express. Ele permite que você escreva funções assíncronas sem precisar usar blocos try/catch em cada rota. Se uma função assíncrona lançar um erro, o asyncHandler captura esse erro e o passa para o middleware de tratamento de erros do Express.
-const asyncHandler = require("express-async-handler");
-
-module.exports = asyncHandler;
